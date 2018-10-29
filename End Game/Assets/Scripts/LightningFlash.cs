b97e5ca5bfefabf2d2d9018d379591f5aa7318dc
CappyGameDev <stirrupprazak.c@gmail.com>
2018-10-29 16:09:47 +1100

    Updated Menu Beta.unity
    Created Menu_House.prefab
    
    Added Cotton Stuffed Menu Theme.mp3
    Added Lightning crack with a huge crisp sound and background torrential rain.wav
    Updated Window.mat
    Added new Tags in TagManager.asset
    
    Created LightningFlash.cs
    Created LightningSource.cs

---
+++ End Game/Assets/Scripts/LightningFlash.cs
@@ -0,0 +1,114 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightningFlash : MonoBehaviour {
+
+    //Specifies The Game Object containing the lights used in lightning flashes
+    [HideInInspector]
+    public GameObject lightningFlash;
+
+    //Checks if the lights have been flashed, as a way of ensuring that the code runs properly
+    private bool flashIsOn;
+
+    public GameObject[] lightningSources;
+    public float[] soundDelays;
+
+    //Used to perform Lightning flashes and controls how long the lights stay on before turning off
+    [Header("Delay between flashes")]
+    public float delayMin;
+    public float delayMax;
+    public float delayTimer = 5;
+
+    [Header("Flash time")]
+    public float flashMin;
+    public float flashMax;
+    public float flashTimer;
+
+    [Header("Lightning base brightness & vol")]
+    public float baseStrengthMin;
+    public float baseStrengthMax;
+    public float soundDelayMin;
+    public float soundDelayMax;
+    public float lightningVolDivider = 5f;
+
+    [Header("Lightning flicker brightness")]
+    public float lightningMin;
+    public float lightningMax;
+    
+
+
+    // make an array of Lightning Sources and turn lights off
+    private void Awake()
+    {
+        foreach (GameObject lightning in lightningSources)
+        {
+            lightning.GetComponent<Light>().intensity = 0;
+        }
+        flashIsOn = false;
+        flashTimer = Random.Range(flashMin, flashMax);
+    }
+
+    
+
+	
+	void Update () {
+		
+        // count down to flash
+        if (delayTimer > 0)
+        {
+            delayTimer -= Time.deltaTime;
+        }
+        
+        // when countdown completes, do flash, set new delay, and invoke delayed StopFlash
+        if (delayTimer <= 0)
+        {
+            delayTimer = Random.Range(delayMin, delayMax);
+            Flash();
+            Invoke("StopFlash", flashTimer);
+        }
+
+        Flicker();
+
+    }
+
+    public void Flash()
+    {
+        flashIsOn = true;
+
+        foreach (GameObject lightning in lightningSources)
+        {
+            lightning.GetComponent<LightningSource>().baseStrength = Random.Range(baseStrengthMin, baseStrengthMax);
+            lightning.GetComponent<LightningSource>().PlayDelayedSound();
+        }
+
+        
+        //Invoke("PlaySound", Random.Range(soundDelayMin, soundDelayMax));
+    }
+
+
+    public void StopFlash()
+    {
+        flashIsOn = false;
+        flashTimer = Random.Range(flashMin, flashMax);
+        foreach (GameObject lightning in lightningSources)
+        {
+            lightning.GetComponent<Light>().intensity = 0;
+        }
+        
+    }
+
+
+    public void Flicker()
+    {
+        if (flashIsOn)
+        {
+            foreach (GameObject lightning in lightningSources)
+            {
+                lightning.GetComponent<Light>().intensity = Random.Range(lightningMin, lightningMax + lightning.GetComponent<LightningSource>().baseStrength);
+            }
+        }
+    }
+
+
+}
---
+++ End Game/Assets/Scripts/LightningSource.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class LightningSource : MonoBehaviour {
+
+    public float baseStrength;
+    private AudioSource audSrc;
+    private LightningFlash lightningManager;
+
+
+    private void Start()
+    {
+        audSrc = GetComponentInChildren<AudioSource>();
+        lightningManager = GameObject.Find("Menu Manager").GetComponent<LightningFlash>();
+    }
+
+
+    public void PlayDelayedSound()
+    {
+        Invoke("PlaySound", Random.Range (lightningManager.soundDelayMin, lightningManager.soundDelayMax));
+    }
+
+
+
+    public void PlaySound()
+    {
+        //randomise volume
+        audSrc.volume = baseStrength / lightningManager.lightningVolDivider;
+        Debug.Log(gameObject.name + " vol = " + audSrc.volume);
+        // randomise pitch
+        audSrc.pitch = Random.Range(0.6f, 1.1f);
+        //play sound
+        audSrc.Play();
+    
+    }
+
+
+}
