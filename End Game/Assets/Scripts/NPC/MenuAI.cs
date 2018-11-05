a90af13ffda26a555fe7b09c84b9b24fabd64a86
edohtron <edohtron@users.noreply.github.com>
2018-11-05 14:45:28 +1100

    NPC patrolling MENU SCENE functional, added related scripts, menu mobs/UI prefabs

--- End Game/Assets/Scripts/NPC/MenuAI.cs
+++ End Game/Assets/Scripts/NPC/MenuAI.cs
@@ -3,48 +3,94 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
-public class MenuAI : NPC
+[RequireComponent(typeof(NavMeshAgent))]
+[RequireComponent(typeof(BoxCollider))]
+[RequireComponent(typeof(Rigidbody))]
+public class MenuAI : MonoBehaviour
 {
+    private MenuMobManager mobManager;
     private float MoveSpeed;
     private float Monstertimer;
     private float BearTimer;
 
+    public bool isWalking;
+
     private NavMeshAgent NMA;
     private Animator animat;
-
-    private GameObject Bear;
-    private GameObject Owl;
-    private GameObject Croc;
+    public GameObject currentDestination;
+    public float distOffset = 5;
 
     // Use this for initialization
     void Start ()
     {
+        mobManager = GameObject.Find("Menu_MobManager").GetComponent<MenuMobManager>();
         NMA = GetComponent<NavMeshAgent>();
         animat = GetComponent<Animator>();
+        currentDestination = null;
 
         Monstertimer = 30;
         BearTimer = Monstertimer;
         MoveSpeed = 3;
 
-        Bear = GameObject.FindGameObjectWithTag("Bear");
-        Owl = GameObject.FindGameObjectWithTag("Owl");
-        Croc = GameObject.FindGameObjectWithTag("Crocodile");
-
-        Bear.gameObject.SetActive(false);
-        Owl.gameObject.SetActive(false);
-        Croc.gameObject.SetActive(false);
 	}
 	
 	// Update is called once per frame
 	void Update ()
     {
-        if (BearTimer != 0)
-        {
-            if(BearTimer >= 0)
-            {
-                Debug.Log("FIIIIIIIIIISH");
-            }
-            BearTimer--;
+        if (this.gameObject.activeInHierarchy && currentDestination == null) {
+            StartPatrolLap();
         }
+
+        //if (NMA.remainingDistance <= NMA.stoppingDistance) {
+        //    GetNextPoint();
+        //}
 	}
+
+    public void GetNextPoint() {
+        if (currentDestination == mobManager.patrolCheckpoint1) {
+            currentDestination = mobManager.patrolCheckpoint2;
+            NMA.destination = currentDestination.transform.position;
+        }
+
+        else if (currentDestination == mobManager.patrolCheckpoint2) {
+            currentDestination = mobManager.patrolCheckpoint3;
+            NMA.destination = currentDestination.transform.position;
+        }
+
+        else if (currentDestination == mobManager.patrolCheckpoint3) {
+            currentDestination = mobManager.StartCheckpoint;
+            NMA.destination = currentDestination.transform.position;
+        }
+    }
+
+    private void StartPatrolLap() {
+        currentDestination = mobManager.patrolCheckpoint1;
+        NMA.destination = currentDestination.transform.position;
+    }
+
+    private void FinishedPatrolLap() {
+        currentDestination = null;
+        gameObject.SetActive(false);
+        mobManager.SetPatrolDelayTimer(4);
+    }
+
+    private void OnTriggerEnter(Collider other) {
+
+        if (currentDestination == mobManager.patrolCheckpoint1 && other.gameObject.name == "CheckPoint1") {
+            GetNextPoint();
+        }
+
+        else if (currentDestination == mobManager.patrolCheckpoint2 && other.gameObject.name == "CheckPoint2") {
+            GetNextPoint();
+        }
+
+        else if (currentDestination == mobManager.patrolCheckpoint3 && other.gameObject.name == "CheckPoint3") {
+            GetNextPoint();
+        }
+
+        else if (currentDestination == mobManager.StartCheckpoint && other.gameObject == mobManager.StartCheckpoint) {
+            FinishedPatrolLap();
+        }
+    }
+
 }
---
+++ End Game/Assets/Scripts/NPC/MenuMobManager.cs
@@ -0,0 +1,97 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuMobManager : MonoBehaviour {
+
+    public GameObject Bear;
+    public GameObject Owl;
+    public GameObject Croc;
+    [SerializeField] private GameObject currentActive;
+
+    public GameObject StartCheckpoint;
+    public GameObject patrolCheckpoint1;
+    public GameObject patrolCheckpoint2;
+    public GameObject patrolCheckpoint3;
+
+    public float startDelayTimer;
+    public bool countingDown;
+
+    // Use this for initialization
+    void Start () {
+
+        currentActive = null;
+
+        Bear.SetActive(false);
+        Owl.SetActive(false);
+        Croc.SetActive(false);
+
+        countingDown = true;
+               
+        startDelayTimer = 3;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (startDelayTimer > 0 && CountingDown()) {
+            startDelayTimer -= Time.deltaTime;
+        }
+
+        if (startDelayTimer <= 0 && CountingDown()) {
+
+            if (currentActive == null) {
+                ToggleActiveMob();
+            }
+
+            else if (currentActive != null) {
+                ToggleActiveMob();
+            }
+
+            countingDown = false;
+    
+        }
+	}
+
+    private void ToggleActiveMob() {
+        if (currentActive == null) {
+            Bear.SetActive(true);
+            currentActive = Bear;
+            Bear.transform.position = StartCheckpoint.transform.position;
+        }
+
+        else if (currentActive == Bear) {
+            Owl.SetActive(true);
+            currentActive = Owl;
+            Owl.transform.position = StartCheckpoint.transform.position;
+        }
+
+        else if (currentActive == Owl) {
+            Croc.SetActive(true);
+            currentActive = Croc;
+            Croc.transform.position = StartCheckpoint.transform.position;
+        }
+
+        else if (currentActive == Croc) {
+            Bear.SetActive(true);
+            currentActive = Bear;
+            Bear.transform.position = StartCheckpoint.transform.position;
+        }
+    }
+    
+    public void SetPatrolDelayTimer(float timer) {
+        startDelayTimer = timer;
+        countingDown = true;
+
+    }
+
+    public bool CountingDown() {
+        if (countingDown) {
+            return true;
+        }
+
+        return false;
+    }
+
+    
+
+}
